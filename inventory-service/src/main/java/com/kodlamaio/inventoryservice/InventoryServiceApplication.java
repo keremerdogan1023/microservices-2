af055a2b340bbdc89aa197cd7b77f065e45f1437
keremerdogan1023 <keremerdogan1023@gmail.com>
2023-05-18 15:14:51 +0300

    Initial commit

---
+++ inventory-service/src/main/java/com/kodlamaio/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,20 @@
+package com.kodlamaio.inventoryservice;
+
+import com.kodlamaio.commonpackage.utils.constants.Paths;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication(scanBasePackages = {Paths.ConfigurationBasePackage, Paths.Inventory.ServiceBasePackage})
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+
+
+}
